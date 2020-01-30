6cac66681b9cec5a1633b7036d4a216b668285a4
Kerri-Ann <kaybee1jam@yahoo.com>
2020-01-30 12:03:42 -0500

    Update userReducer.js

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -3,10 +3,10 @@ import constants from "../constants";
 const initialState = {};
 
 const userReducer = (state = initialState, { type, payload }) => {
-   switch (type) {
-     default:
-       return state;
-   }
- };
+  switch (type) {
+    default:
+      return state;
+  }
+};
 
 export default userReducer;
