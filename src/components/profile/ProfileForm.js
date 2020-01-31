86bd609d1fd6cc605323b88beefe67d1b180db57
Kerri-Ann <kaybee1jam@yahoo.com>
2020-01-31 13:44:38 -0500

    Base layout for form

--- src/components/profile/ProfileForm.js
+++ src/components/profile/ProfileForm.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { Form, Icon, Input, Button, Row, Col } from 'antd';
+import 'antd/dist/antd.css';
 
 function ProfileForm() {
    return (
-      <div>
-         <p>Profile form...</p>
+      <div className="profile-information">
+         <h1>Update Profile Information</h1>
+         <div className="form-container">
+            <Form layout="vertical">
+               <Row>
+                  <Col span={12}>
+                     <Form.Item label="First Name">
+                        <Input placeholder="First Name" />
+                     </Form.Item>
+                     <Form.Item label="Last Name">
+                        <Input placeholder="Last Name" />
+                     </Form.Item>
+                     <Form.Item label="Email">
+                        <Input placeholder="Email" />
+                     </Form.Item>
+                     <Form.Item label="Website">
+                        <Input placeholder="Website" />
+                     </Form.Item>
+                     <Form.Item label="Bio">
+                        <Input placeholder="Bio" />
+                     </Form.Item>
+                  </Col>
+                  <Col span={12}>
+                     <Form.Item label="Current Password">
+                        <Input placeholder="Current Password" />
+                     </Form.Item>
+                     <Form.Item label="New Password">
+                        <Input placeholder="New Password" />
+                     </Form.Item>
+                     <Form.Item label="Confirm Password">
+                        <Input placeholder="Confirm Password" />
+                     </Form.Item>
+                  </Col>
+                  <Col span={24}>
+                     <Button type="primary" htmlType="submit">
+                        Save
+                     </Button>
+                     <Button type="secondary">
+                        Cancel
+                     </Button>
+                  </Col>
+               </Row>
+            </Form>
+         </div>
       </div>
    )
 }
