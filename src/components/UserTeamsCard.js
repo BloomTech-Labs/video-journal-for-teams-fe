1630a2d9ec7a871b0f303e73cf947abf6d6fef59
Darrin Lowery <lambda@monsto.com>
2020-02-01 11:49:58 -0600

    .

--- src/components/UserTeamsCard.js
+++ src/components/UserTeamsCard.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Card } from 'antd';
+import { Card, Icon } from 'antd';
 
 
 const TeamCard = props => {
 
-    return (
-        <Card className="teamCard" size="small">
-                <p>{props.data.name}</p>
-                <p className="small">{props.data.description}</p>
-        </Card>
-    );
+	return (
+		<Card className="teamCard" size="small"
+			actions={[
+				"",
+				<Icon type="setting" key="setting" title="Manage this team"/>,
+			]}
+		>
+			<p>{props.data.name}</p>
+			<p className="small">{props.data.description}</p>
+		</Card>
+	);
 };
 
 const mapStateToProps = state => {
-    return {
-        isUpdating: state.isUpdating
-    };
+	return {
+		isUpdating: state.isUpdating
+	};
 };
 export default connect(mapStateToProps, {})(TeamCard);
