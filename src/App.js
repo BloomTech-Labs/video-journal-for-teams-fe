49393acbe7452d6ec67ab062ac5fb402606d6430
Michael Nunes <tinysquid.git@gmail.com>
2020-02-03 15:46:43 -0800

    Add global default to axios requests

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import 'antd/dist/antd.css';
-import './userdash.css';
-
+import "./components/utils/AxiosDefaults";
 // Components
 import PrivateRoute from "./components/utils/PrivateRoute";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import TestComponent from "./components/TestComponent"
-import UserDashboard from './pages/UserDashboard';
+import TestComponent from "./components/TestComponent";
+import UserDashboard from "./pages/UserDashboard";
 import UserProfileDashboard from "./pages/UserProfileDashboard";
 
+// Styles
+import "antd/dist/antd.css";
+import "./userdash.css";
+
 function App() {
-    return (
-        <div className="app">
-            
-            <Route exact path="/" component={Login} />
+  return (
+    <div className="app">
+      <Route exact path="/" component={Login} />
 
-            <Route path="/register" component={Register} />
+      <Route path="/register" component={Register} />
 
-            <PrivateRoute path="/test" component={TestComponent} />
+      <PrivateRoute path="/test" component={TestComponent} />
 
-            <Route path="/user-dashboard" component={UserDashboard} />
+      <Route path="/user-dashboard" component={UserDashboard} />
 
-            <Route path="/profile" component={UserProfileDashboard} />
-
-        </div>
-    );
+      <Route path="/profile" component={UserProfileDashboard} />
+    </div>
+  );
 }
 
 export default App;
