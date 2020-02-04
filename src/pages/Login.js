6b6d26f43cfe498dbe7cd545910ccd8694a32a8b
Michael Nunes <tinysquid.git@gmail.com>
2020-02-04 15:05:11 -0800

    Hookup login page to use redux and yup for validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,98 +1,119 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+import * as yup from "yup";
+
+// Redux
+import { connect } from "react-redux";
+
+// Actions
+import { loginUser, setError, clearError } from "../redux/actions/userActions";
 
 // Components
-import { Layout, Form, Icon, Input, Button, Checkbox } from "antd";
+import { Layout, Form, Icon, Input, Button, Checkbox, Alert } from "antd";
 
 // Styles
 import "../components/profile/tempStyles.css";
 
 const { Sider, Content } = Layout;
 
+const emailLoginSchema = yup.object().shape({
+  email: yup.string().email(),
+});
+
 const Login = (props) => {
-  const [user, setUser] = useState("");
-  const [pass, setPass] = useState("");
+  const [user, setUser] = useState({
+    usernameOrEmail: "",
+    password: "",
+  });
 
-  const userHandler = (e) => {
-    setUser(e.target.value);
-  };
-
-  const passHandler = (e) => {
-    setPass(e.target.value);
-  };
-
-  const login = (e) => {
-    e.preventDefault();
-    if (user.includes("@")) {
-      const credentials = {
-        email: user,
-        password: pass,
-      };
-      axios
-        .post("/api/auth/login/email", credentials)
-        .then((res) => {
-          localStorage.setItem("token", res.data.token);
-          props.history.push("/user-dashboard");
-        })
-        .catch((err) => console.log(err));
-    } else {
-      const credentials = {
-        username: user,
-        password: pass,
-      };
-      axios
-        .post("/auth/login/username", credentials)
-        .then((res) => {
-          localStorage.setItem("token", res.data.token);
-          props.history.push("/user-dashboard");
-        })
-        .catch((err) => console.log({ err }));
+  //Redirect if logged already logged in OR on successful registration
+  useEffect(() => {
+    if (props.isLogged) {
+      props.clearError();
+      props.history.push("/user-dashboard");
     }
+  }, [props.isLogged]);
+
+  const handleInput = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value.trim() });
+  };
+
+  const submitLogin = (e) => {
+    e.preventDefault();
+
+    const userCredentials = user;
+
+    emailLoginSchema
+      .validate(user.usernameOrEmail)
+      .then(() => {
+        userCredentials.method = "email";
+
+        props.loginUser(userCredentials);
+      })
+      .catch(() => {
+        userCredentials.method = "username";
+
+        props.loginUser(userCredentials);
+      });
+
+    setUser({ usernameOrEmail: "", password: "" });
   };
 
   return (
-    <div>
-      <Layout>
-        <Sider />
-        <Content>
-          <Form onSubmit={login} className="login-form">
-            <Form.Item>
-              <Input
-                prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.5)" }} />}
-                value={user}
-                onChange={userHandler}
-                placeholder="Enter username or email"
-                autoComplete="off"
-                required
-              />
-            </Form.Item>
-            <Form.Item>
-              <Input
-                prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.5)" }} />}
-                type="password"
-                value={pass}
-                onChange={passHandler}
-                placeholder="Enter password"
-                autoComplete="off"
-                required
-              />
-            </Form.Item>
-            <Form.Item>
-              <Checkbox>Remember me</Checkbox>
-              <Link to="" className="login-form-forgot">
-                forgot password?
-              </Link>
-              <Button type="primary" htmlType="submit" className="login-form-button">
-                Log In
-              </Button>
-              <Link to="/register">or register now!</Link>
-            </Form.Item>
-          </Form>
-        </Content>
-      </Layout>
-    </div>
+    <Layout>
+      <Sider />
+      <Content>
+        {props.error ? <Alert message={props.error} type="error" /> : null}
+        <Form onSubmit={submitLogin} className="login-form">
+          <Form.Item>
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.5)" }} />}
+              type="text"
+              name="usernameOrEmail"
+              value={user.usernameOrEmail}
+              onChange={handleInput}
+              placeholder="Username or email"
+              autoComplete="off"
+              required
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.5)" }} />}
+              type="password"
+              name="password"
+              value={user.password}
+              onChange={handleInput}
+              placeholder="Password"
+              autoComplete="off"
+              required
+            />
+          </Form.Item>
+          <Form.Item>
+            <Checkbox>Remember me</Checkbox>
+            <Link to="" className="login-form-forgot">
+              Forgot password?
+            </Link>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log In
+            </Button>
+            <Link to="/register">Register an account instead</Link>
+          </Form.Item>
+        </Form>
+      </Content>
+    </Layout>
   );
 };
 
-export default Login;
+const mapStateToProps = (state) => ({
+  isLogged: state.User.isLogged,
+  error: state.User.error,
+});
+
+const mapActionsToProps = {
+  loginUser,
+  setError,
+  clearError,
+};
+
+export default connect(mapStateToProps, mapActionsToProps)(Login);
