a4b5b9501dcfeb9aea83d9bb3986577f1000a692
Michael Nunes <tinysquid.git@gmail.com>
2020-02-05 16:08:59 -0800

    Alter video card to navigate to video details on click

--- src/components/UserVideosCard.js
+++ src/components/UserVideosCard.js
@@ -1,32 +1,35 @@
-import React from 'react'
-import { Card, Row, Col } from 'antd';
-import 'antd/dist/antd.css';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { Card, Row, Col } from "antd";
+import "antd/dist/antd.css";
 
 const UserVideosCard = (props) => {
-	const data = props.data;
-	return (
-		<Card className="videoCard" size="small">
-			<Row gutter={[16, 16]}>
-				<Col span={6}>
-					<img src={data.fs_path} height={100} width={100} />
-				</Col>
-				<Col span={3}>
-					{/* Space only */}
-				</Col>
-				<Col span={15} style={{ textAlign: "left" }}>
-						<p>{data.title}</p>
-						{data.description && <p className="small">{data.description}</p>}
-						<p className="small">{data.created_at}</p>
-				</Col>
-			</Row>
-		</Card>
-		// <Layout>
-		// 		<Col span={12} style={{ textAlign: "left" }}>
-		// 			<p>Feedbacks go here?</p>
-		// 		</Col>
-		// 	</Row>
-		// </Layout>
-	)
-}
+  const data = props.data;
+  return (
+    <Card
+      className="videoCard"
+      size="small"
+      style={{ cursor: "pointer" }}
+      onClick={() => props.history.push(`/user-dashboard/video/${data.id}`)}>
+      <Row gutter={[16, 16]}>
+        <Col span={6}>
+          <img src={data.fs_path} height={100} width={100} />
+        </Col>
+        <Col span={3}>{/* Space only */}</Col>
+        <Col span={15} style={{ textAlign: "left" }}>
+          <p>{data.title}</p>
+          {data.description && <p className="small">{data.description}</p>}
+          <p className="small">{data.created_at}</p>
+        </Col>
+      </Row>
+    </Card>
+    // <Layout>
+    // 		<Col span={12} style={{ textAlign: "left" }}>
+    // 			<p>Feedbacks go here?</p>
+    // 		</Col>
+    // 	</Row>
+    // </Layout>
+  );
+};
 
-export default UserVideosCard
+export default withRouter(UserVideosCard);
