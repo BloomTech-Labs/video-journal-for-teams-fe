5cb8eca08ef8ce9493e7575cf285c6fab34e9fa6
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-02-10 15:40:51 -0800

    added invite link on team dashboard

--- src/components/team/MembersList.js
+++ src/components/team/MembersList.js
@@ -1,53 +1,82 @@
-import React, { useEffect } from 'react';
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useParams, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
-import { fetchTeamMembers, setError, clearError } from "../../redux/actions/teamActions";
-import { Layout, Typography, Row, Col, Button } from 'antd';
+import { fetchTeamById, fetchTeamMembers, createInvite, setError, clearError } from "../../redux/actions/teamActions";
+import { Layout, Typography, Row, Col, Modal, Button, Form, Input } from 'antd';
 import './teamTest.css';
 import MemberCard from './MemberCard';
 
 const { Header, Content } = Layout;
 
 function MembersList(props) {
+	let history = useHistory();
 	let { team_id } = useParams();
 
+	const [showModal, setShowModal] = useState(false)
+
 	useEffect(() => {
-			props.fetchTeamMembers(team_id)
+		props.fetchTeamById(team_id)
+		props.fetchTeamMembers(team_id)
 	}, []);
 
-  if (!props.teamMembers) {
-    return <h2>Loading...</h2>;
-  } else {
+	const toggleModal = () => {
+		setShowModal(!showModal)
+		props.createInvite({ team_id: props.team.id, team_name: props.team.name })
+	}
 
-    return (
-      <Content>
-        <p>Members({props.teamMembers.length})</p>
-        <Row gutter={[16, 16]}>
-          {/* Add member button */}
-          <Col span={2}>
-            <Button type="primary" shape="circle" icon="plus-circle" className="add-member" />
-          </Col>
+	const handleOk = (e) => {
+		toggleModal();
+	}
 
-          {/* Display members */}
-          {props.teamMembers.map(member => (
-            <Col span={2} key={member.user_id}><MemberCard  member={member} /></Col>
-          ))}
-        </Row>
-      </Content>
-    )
-  }
+	if (!props.teamMembers) {
+		return <h2>Loading...</h2>;
+	} else {
+
+		return (
+			<Content>
+				<p>Members({props.teamMembers.length})</p>
+				<Row gutter={[16, 16]}>
+					{/* Add member invite link button */}
+					<Col span={2}>
+						<Button onClick={toggleModal} type="primary" shape="circle" icon="plus-circle" className="add-member" />
+						<Modal
+							title="Team Invitation Link"
+							visible={showModal}
+							onOk={handleOk}
+							onCancel={toggleModal}
+							okText="Okay"
+						>
+							<Form>
+								<Form.Item label="Copy this code link">
+									<Input readOnly value={props.inviteLink.link} />
+								</Form.Item>
+							</Form>
+						</Modal>
+					</Col>
+
+					{/* Display members */}
+					{props.teamMembers.map(member => (
+						<Col span={2} key={member.user_id}><MemberCard member={member} /></Col>
+					))}
+				</Row>
+			</Content>
+		)
+	}
 }
 
 const mapStateToProps = (state) => ({
-  team: state.Team.team,
+	team: state.Team.team,
 	teamMembers: state.Team.teamMembers,
+	inviteLink: state.Team.inviteLink,
 	deleteUserCount: state.Team.deleteUserCount
 });
 
 const mapActionsToProps = {
-  fetchTeamMembers,
-  setError,
-  clearError
+	fetchTeamById,
+	fetchTeamMembers,
+	createInvite,
+	setError,
+	clearError
 };
 
 export default connect(mapStateToProps, mapActionsToProps)(MembersList);
--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,57 +1,61 @@
 const constants = {
-  //Syntax of a constant CONST_NAME: "reducer-name:action-name"
-  DEMO_CONSTANT: "demo:get-demo-data",
+	//Syntax of a constant CONST_NAME: "reducer-name:action-name"
+	DEMO_CONSTANT: "demo:get-demo-data",
 
-  //USER CONSTANTS
-    //AUTH
-    REGISTER_USER: "user:register-user",
-    LOGIN_USER: "user:login",
-    LOGOUT_USER: "user:logout",
+	//USER CONSTANTS
+	//AUTH
+	REGISTER_USER: "user:register-user",
+	LOGIN_USER: "user:login",
+	LOGOUT_USER: "user:logout",
 
-    //GET fetch teams for user
-    FETCH_USER_TEAMS_START: "user:fetch-teams-start",
-    FETCH_USER_TEAMS_SUCCESS: "user:fetch-teams-success",
+	//GET fetch teams for user
+	FETCH_USER_TEAMS_START: "user:fetch-teams-start",
+	FETCH_USER_TEAMS_SUCCESS: "user:fetch-teams-success",
 
-    //GET fetch videos for user
-    FETCH_USER_VIDEOS_START: "user:fetch-videos-start",
-    FETCH_USER_VIDEOS_SUCCESS: "user:fetch-videos-success",
+	//GET fetch videos for user
+	FETCH_USER_VIDEOS_START: "user:fetch-videos-start",
+	FETCH_USER_VIDEOS_SUCCESS: "user:fetch-videos-success",
 
-  FETCH_VIDEO_START: "user:fetch-video-start",
-  FETCH_VIDEO_SUCCESS: "user:fetch-video-success",
-  FETCH_VIDEO_FAILURE: "user:fetch-video-failure",
+	FETCH_VIDEO_START: "user:fetch-video-start",
+	FETCH_VIDEO_SUCCESS: "user:fetch-video-success",
+	FETCH_VIDEO_FAILURE: "user:fetch-video-failure",
 
-  FETCH_FEEDBACK_START: "user:fetch-feedback-start",
-  FETCH_FEEDBACK_SUCCESS: "user:fetch-feedback-success",
-  FETCH_FEEDBACK_FAILURE: "user:fetch-feedback-failure",
+	FETCH_FEEDBACK_START: "user:fetch-feedback-start",
+	FETCH_FEEDBACK_SUCCESS: "user:fetch-feedback-success",
+	FETCH_FEEDBACK_FAILURE: "user:fetch-feedback-failure",
 
-  //TEAM CONSTANTS
-    // POST create team
-    CREATE_TEAM_START: "teams:create-team-start",
-    CREATE_TEAM_SUCCESS: "teams:create-team-success",
+	//TEAM CONSTANTS
+	// POST create team
+	CREATE_TEAM_START: "teams:create-team-start",
+	CREATE_TEAM_SUCCESS: "teams:create-team-success",
 
-    //GET fetch team by id
-    FETCH_TEAM_BY_ID_START: "team:fetch-teamById-start",
-    FETCH_TEAM_BY_ID_SUCCESS: "team:fetch-teamById-success",
-  
-    //GET fetch team members
-    FETCH_TEAM_MEMBERS_START: "team:fetch-team-members-start",
-    FETCH_TEAM_MEMBERS_SUCCESS: "team:fetch-team-members-success",
+	//GET fetch team by id
+	FETCH_TEAM_BY_ID_START: "team:fetch-teamById-start",
+	FETCH_TEAM_BY_ID_SUCCESS: "team:fetch-teamById-success",
 
-    //GET fetch team prompts
-    FETCH_TEAM_PROMPTS_START: "team:fetch-team-prompts-start",
-    FETCH_TEAM_PROMPTS_SUCCESS: "team:fetch-team-prompts-success",
+	//GET fetch team members
+	FETCH_TEAM_MEMBERS_START: "team:fetch-team-members-start",
+	FETCH_TEAM_MEMBERS_SUCCESS: "team:fetch-team-members-success",
 
-    //GET fetch team videos
-    FETCH_TEAM_VIDEOS_START: "team:fetch-team-videos-start",
-    FETCH_TEAM_VIDEOS_SUCCESS: "team:fetch-team-videos-success",
+	//GET fetch team prompts
+	FETCH_TEAM_PROMPTS_START: "team:fetch-team-prompts-start",
+	FETCH_TEAM_PROMPTS_SUCCESS: "team:fetch-team-prompts-success",
 
-    //DELETE team member from a team
-    DELETE_TEAM_MEMBER_START: "team:delete-team-member-start",
-    DELETE_TEAM_MEMBER_SUCCESS: "team:delete-team-member-success",
+	//GET fetch team videos
+	FETCH_TEAM_VIDEOS_START: "team:fetch-team-videos-start",
+	FETCH_TEAM_VIDEOS_SUCCESS: "team:fetch-team-videos-success",
 
-  //FAILURES
-  GENERATE_ERROR: "error:generate-error",
-  CLEAR_ERROR: "error:clear-error",
+	//DELETE team member from a team
+	DELETE_TEAM_MEMBER_START: "team:delete-team-member-start",
+	DELETE_TEAM_MEMBER_SUCCESS: "team:delete-team-member-success",
+
+	//POST creates team invitation link
+	POST_INVITE_LINK_START: "team:post-invite-link-start",
+	POST_INVITE_LINK_SUCCESS: "team:post-invite-link-success",
+
+	//FAILURES
+	GENERATE_ERROR: "error:generate-error",
+	CLEAR_ERROR: "error:clear-error",
 };
 
 export default constants;
