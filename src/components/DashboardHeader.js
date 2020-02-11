2e36fba050c5458f66dd7195638d95767442d2b7
Michael Nunes <tinysquid.git@gmail.com>
2020-02-11 10:06:06 -0800

    Update dash header & avatar with fullname & username

--- src/components/DashboardHeader.js
+++ src/components/DashboardHeader.js
@@ -1,45 +1,46 @@
-import React from 'react';
+import React from "react";
 import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { Layout, Tooltip, Avatar, Typography, Button, Icon} from 'antd';
+import { Layout, Tooltip, Avatar, Typography, Button, Icon } from "antd";
 import { logoutUser } from "../redux/actions/userActions";
 
 const { Title } = Typography;
 const { Header, Content } = Layout;
 
 function DashboardHeader(props) {
-  let history = useHistory();
+	let history = useHistory();
 
 	const handleLogout = (e) => {
 		e.preventDefault();
 		props.logoutUser();
 		history.push("/");
-	}
+	};
 
-  return (
-    <Header className="userDashHeader">
-				<div className="userDashContentHeader">
-					<Title level={4}>{props.username}</Title>
-					<Tooltip placement="left" title="username here">
-						{/* change src below for image */}
-						<Avatar size="large" icon="user" src="" />
-					</Tooltip>
-				</div>
+	return (
+		<Header className="userDashHeader">
+			<div className="userDashContentHeader">
+				<Title level={4}>{props.fullName}</Title>
+				<Tooltip placement="left" title={props.username}>
+					{/* change src below for image */}
+					<Avatar size="large" icon="user" src="" />
+				</Tooltip>
+			</div>
 			<Button onClick={handleLogout}>
-				<Icon type="logout" style={{ fontSize: '32px' }} />
+				<Icon type="logout" style={{ fontSize: "32px" }} />
 			</Button>
 		</Header>
-  )
+	);
 }
 
 const mapStateToProps = (state) => {
-  return {
-    username: state.User.username,
-  }
-}
+	return {
+		fullName: `${state.User.first_name} ${state.User.last_name}`,
+		username: state.User.username,
+	};
+};
 
 const mapActionsToProps = {
-  logoutUser
+	logoutUser,
 };
 
 export default connect(mapStateToProps, mapActionsToProps)(DashboardHeader);
