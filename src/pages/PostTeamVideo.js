64a3372f23ae83ee7dda8fe2bfeac010fa10b0d2
Michael Nunes <tinysquid.git@gmail.com>
2020-02-13 14:30:41 -0800

    Blockout elements and setup form functions

--- src/pages/PostTeamVideo.js
+++ src/pages/PostTeamVideo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 
 // Redux
 import { connect } from "react-redux";
@@ -8,7 +8,7 @@ import { connect } from "react-redux";
 // Components
 import DashboardHeader from "../components/DashboardHeader";
 import DashboardNav from "../components/DashboardNav";
-import { Layout, Card, Button } from "antd";
+import { Layout, Card, Button, Icon, Form } from "antd";
 
 // Styles
 import "../userdash.css";
@@ -26,13 +26,65 @@ const { Content } = Layout;
 //* Ability to upload video to server and post to a team
 
 function PostTeamVideo(props) {
+	const playbackContainer = useRef(null);
+	const blobStreamHandle = useRef(null);
+
+	function downloadVideo(e) {
+		e.preventDefault();
+
+		console.log("Download video");
+	}
+
+	function uploadVideo(e) {
+		e.preventDefault();
+
+		console.log("Upload video");
+	}
+
 	return (
 		<Layout>
 			<DashboardNav />
 			<Content>
 				<DashboardHeader />
 				<Card title="Record and post a new video" style={{ margin: "20px" }}>
-					<p>Yeet</p>
+					<div className="record-container">
+						<a ref={blobStreamHandle} id="blob-stream" href="#" hidden></a>
+						<div className="record-controls">
+							<Button id="start" style={{ marginRight: "8px" }}>
+								Start Recording
+							</Button>
+							<Button id="stop" style={{ marginRight: "8px" }}>
+								Stop Recording
+							</Button>
+							<Button id="pause" style={{ marginRight: "8px" }}>
+								Pause Recording
+							</Button>
+							<Button id="continue">Continue Recording</Button>
+						</div>
+
+						<video id="liveFeed" width="560" height="315"></video>
+					</div>
+					<div ref={playbackContainer} className="playback-container">
+						<video id="recordedStream" width="560" height="315" controls></video>
+
+						<div className="playback-file-options" style={{ marginTop: "16px" }}>
+							<Form onSubmit={downloadVideo}>
+								<Form.Item>
+									<Button style={{ marginRight: "8px" }} htmlType="submit">
+										<Icon type="download" /> Download Video
+									</Button>
+								</Form.Item>
+							</Form>
+
+							<Form onSubmit={uploadVideo}>
+								<Form.Item>
+									<Button htmlType="submit">
+										<Icon type="upload" /> Upload Video
+									</Button>
+								</Form.Item>
+							</Form>
+						</div>
+					</div>
 				</Card>
 			</Content>
 		</Layout>
