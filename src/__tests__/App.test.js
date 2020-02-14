fa240f7be5cd4f216af76c7b5b442f675959fc9d
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2020-02-13 23:24:50 -0800

    Initial tests for Register.js component.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,12 +1,22 @@
 import React from "react";
 import * as rtl from "@testing-library/react";
+// Redux
+import { Provider } from "react-redux";
+// import { persistor, store } from "./redux/store";
+import { store } from "../redux/store";
+// import { PersistGate } from 'redux-persist/lib/integration/react';
+import { BrowserRouter as Router } from "react-router-dom";
+
 import App from "../App";
 
-//afterEach(rtl.cleanup); //Use if necessary, see docs
-//https://jestjs.io/docs/en/setup-teardown
-
-describe("Main App Demo Test", () => {
-  it("renders without crashing", () => {
-    rtl.render(<App />);
-  });
+describe("App Component", () => {
+	it("should render without crashing", () => {
+		rtl.render(
+			<Provider store={store}>
+				<Router>
+					<App />
+				</Router>
+			</Provider>
+		);
+	});
 });
