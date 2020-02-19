489f05b97c2d7914eb977f3ef9cfb55f35cf1bf6
Darrin Lowery <lambda@monsto.com>
2020-02-19 16:21:45 -0600

    .

--- src/components/user/UserVideosCard.js
+++ src/components/user/UserVideosCard.js
@@ -9,11 +9,7 @@ const UserVideosCard = (props) => {
 	const data = props.data;
 	return (
 		<Card className="video-card"
-			cover={
-				<img
-					src={data.thumbnail}
-				/>
-			}
+			cover={<img src={data.thumbnail}/>}
 			actions={[
 				<Icon type="setting" key="setting" />,
 				<Icon type="edit" key="edit" />,
@@ -22,28 +18,9 @@ const UserVideosCard = (props) => {
 		>
 			<Meta style={{ textAlign: "left" }}
 				title={data.title}
-				description={data.description && <span className="small">{data.description}{data.description}{data.description}</span>}
+				description={data.description && <span className="small">{data.description}</span>}
 			/>
 		</Card>
-		// <Card className="video-card" size="small" onClick={() => props.history.push(`/user-dashboard/video/${data.id}`)}>
-		// 	<Row className="video-card-inner" gutter={[8, 8]}>
-		// 		<Col span={8}>
-		// 			<img src={data.thumbnail} height={100} width={100} />
-		// 		</Col>
-		// 		<Col span={3}>{/* Space only */}</Col>
-		// 		<Col span={8} style={{ textAlign: "left" }}>
-		// 			<p>{data.title}</p>
-		// 			{data.description && <p className="small">{data.description}</p>}
-		// 			<p className="small">{data.created_at}</p>
-		// 		</Col>
-		// 	</Row>
-		// </Card>
-		// <Layout>
-		// 		<Col span={12} style={{ textAlign: "left" }}>
-		// 			<p>Feedbacks go here?</p>
-		// 		</Col>
-		// 	</Row>
-		// </Layout>
 	);
 };
 
