c2bf83fc791d9f8ac8752081eb96e24b7aa8afc3
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-02-23 08:48:00 -0800

    added form validation, made prompt modal component

--- src/components/team/PromptVideoList.js
+++ src/components/team/PromptVideoList.js
@@ -1,40 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { useParams, useHistory } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+//Redux
 import { connect } from "react-redux";
 import { fetchTeamVideos, createPrompt, setError, clearError } from "../../redux/actions/teamActions";
-import { Layout, Card, Row, Col, Modal, Form, Input, Button, Collapse, Divider } from "antd";
-import "./teamTest.css";
+
+
+//Components
+import PromptModal from "./PromptModal.js";
 import PromptCard from "./PromptCard.js";
 
-const { Header, Content } = Layout;
+//Styling
+import { Layout, Button, Collapse, Divider } from "antd";
+import "./teamTest.css";
+const { Content } = Layout;
 const { Panel } = Collapse;
 
 const PromptVideoList = (props) => {
 	// #region CLICK UNCOLLAPSE ICON TO SHOW COMPONENT LOGIC
 	const [showModal, setShowModal] = useState(false);
-	const [prompt, setPrompt] = useState({ question: "", description: "" });
-
 	let { team_id } = useParams();
-	let history = useHistory();
 
 	useEffect(() => {
 		props.fetchTeamVideos(team_id);
 	}, [team_id, props.newPrompt]);
 
-	const toggleModal = () => {
-		setShowModal(!showModal);
-	};
-
-	const handleOk = (e) => {
-		toggleModal();
-		props.createPrompt(prompt, team_id);
-	};
-
-	const handleInput = (e) => {
-		setPrompt({ ...prompt, [e.target.name]: e.target.value });
-	};
 	//#endregion CLICK TO UNCOLLAPSE COMPONENT LOGIC
-
 	if (!props.teamPromptsAndVideos) {
 		return <h2>Loading...</h2>;
 	} else {
@@ -42,21 +33,22 @@ const PromptVideoList = (props) => {
 			<Content>
 				<h1>Prompts({props.teamPromptsAndVideos.length})</h1>
 				{props.userRole === 1 ? null : (
-					<Button onClick={toggleModal} type="primary" shape="round" icon="plus" className="adding-button">
+					<Button
+						type="primary"
+						shape="round"
+						icon="plus"
+						className="adding-button"
+						onClick={() => setShowModal(true)}>
 						New Prompt
 					</Button>
 				)}
 				<Divider />
-				<Modal title="Add New Prompt" visible={showModal} onOk={handleOk} onCancel={toggleModal} okText="Submit">
-					<Form>
-						<Form.Item label="Question">
-							<Input onChange={handleInput} name="question" placeholder="required" />
-						</Form.Item>
-						<Form.Item label="Description">
-							<Input onChange={handleInput} name="description" placeholder="optional" />
-						</Form.Item>
-					</Form>
-				</Modal>
+				<PromptModal
+					isVisible={showModal}
+					setVisibility={setShowModal}
+					createPrompt={props.createPrompt}
+					teamId={props.teamId}
+				/>
 				{props.teamPromptsAndVideos.map((prompt) => (
 					<PromptCard key={prompt.id} data={prompt} />
 				))}
