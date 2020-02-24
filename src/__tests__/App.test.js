1d5ac7b00c1a44c6d0ab7972bf9f446de535a579
Michael Nunes <tinysquid.git@gmail.com>
2020-02-24 08:28:30 -0800

    Update App tests to use shallow rendering

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,23 +1,22 @@
 import React from "react";
-import * as rtl from "@testing-library/react";
+// import * as rtl from "@testing-library/react";
+import { shallow } from "enzyme";
 
 // Redux
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 // import { persistor, store } from "./redux/store";
-import { store } from "../redux/store";
+// import { store } from "../redux/store";
 // import { PersistGate } from 'redux-persist/lib/integration/react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 
 import App from "../App";
 
 describe("App Component", () => {
 	it("should render without crashing", () => {
-		rtl.render(
-			<Provider store={store}>
-				<Router>
-					<App />
-				</Router>
-			</Provider>
+		shallow(
+			<MemoryRouter>
+				<App />
+			</MemoryRouter>
 		);
 	});
 });
