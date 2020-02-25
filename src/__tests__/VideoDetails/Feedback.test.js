b082595992d01591b87f0a4f591623c8539ba64e
Michael Nunes <tinysquid.git@gmail.com>
2020-02-25 12:39:41 -0800

    Add tests for Feedback component

--- src/__tests__/VideoDetails/Feedback.test.js
+++ src/__tests__/VideoDetails/Feedback.test.js
@@ -1,19 +1,90 @@
 import React from "react";
 
+// Mock redux store
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+
 import { shallow, mount } from "enzyme";
 
-import Feedback from "../../components/VideoDetails/Feedback";
+import { Feedback } from "../../components/VideoDetails/Feedback";
+import { FeedbackForm } from "../../components/VideoDetails/FeedbackForm";
+import FeedbackTable from "../../components/VideoDetails/FeedbackTable";
 
-describe("Feedback Component", () => {
-	test("should render without crashing", () => {
-		shallow(<Feedback />);
+const mockStore = configureStore({});
+
+describe("<Feedback>", () => {
+	let store;
+	let actions;
+	let wrapper;
+
+	beforeAll(() => {
+		store = mockStore({
+			User: {
+				videoDetailFocus: {
+					feedback: {
+						isSubmitting: false,
+					},
+				},
+			},
+		});
+
+		actions = {
+			fetchFeedback: jest.fn(),
+		};
 	});
 
-	test("should have table", () => {
-		const wrapper = mount(<Feedback />);
+	afterEach(() => {
+		wrapper.unmount();
 	});
 
-	test("should have 2 dummy posts", () => {
-		const wrapper = mount(<Feedback />);
+	test("should render self without error", () => {
+		wrapper = shallow(<Feedback />);
+	});
+
+	test("should render <FeedbackForm> if viewer is not owner", () => {
+		const props = {
+			loggedInUserId: 2,
+			videoOwnerId: 1,
+		};
+
+		wrapper = mount(
+			<Provider store={store}>
+				<Feedback {...props} />
+			</Provider>
+		);
+
+		expect(wrapper.exists(FeedbackForm)).toBe(true);
+	});
+
+	test("should render <FeedbackTable> if viewer is owner", () => {
+		const props = {
+			loggedInUserId: 1,
+			videoOwnerId: 1,
+		};
+
+		wrapper = mount(
+			<Provider store={store}>
+				<Feedback {...props} fetchFeedback={jest.fn()} />
+			</Provider>
+		);
+
+		expect(wrapper.exists(FeedbackTable)).toBe(true);
+	});
+
+	test("should fetch feedback for <FeedbackTable> if viewer is owner", () => {
+		const props = {
+			loggedInUserId: 1,
+			videoOwnerId: 1,
+		};
+
+		wrapper = mount(
+			<Provider store={store}>
+				<Feedback {...props} {...actions} />
+			</Provider>
+		);
+
+		expect(wrapper.exists(FeedbackTable)).toBe(true);
+
+		expect(actions.fetchFeedback.mock.calls.length).toBe(1);
 	});
 });
