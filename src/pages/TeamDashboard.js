c1961d4550209a331d130a0e0ef1dd010b354e15
Kerri-Ann <kaybee1jam@yahoo.com>
2020-02-25 15:58:27 -0500

    redirects user from forbidden page after 10 secs. complete with no errors

--- src/pages/TeamDashboard.js
+++ src/pages/TeamDashboard.js
@@ -16,13 +16,39 @@ function TeamDashboard({ team, fetchTeamById, fetchTeamMembers, teamMembers, use
 	const [count, setCount] = useState(10);
 	let { team_id } = useParams();
 	const history = useHistory();
+	let redirectTimer = null;
+	let countTimer = null;
 
 	useEffect(() => {
 		clearError();
 		fetchTeamById(team_id);
 		fetchTeamMembers(team_id);
 		fetchTeamVideos(team_id);
+
 	}, [team_id, fetchTeamById, fetchTeamMembers]);
+
+	// Check if there is an error on mount.
+	useEffect(() => {
+
+		if (!isFetching && teamError) {
+			// If error status is forbidden, redirect user after 10 secs.
+			if (teamError.status === 403) {
+				redirectTimer = setTimeout(() => {
+					// Redirect after 10 secs...
+					clearTimeout(redirectTimer);
+					history.push('/user-dashboard');
+					clearError();
+				}, 10000)
+
+			}
+		}
+
+		// Cleanup set timout
+		return () => {
+			clearTimeout(redirectTimer);
+			clearTimeout(countTimer);
+		}
+	}, [teamError]);
 
 	// Sets the logged in user role for the team
 	useEffect(() => {
@@ -32,31 +58,30 @@ function TeamDashboard({ team, fetchTeamById, fetchTeamMembers, teamMembers, use
 		}
 	}, [teamMembers, userId])
 
+	const clearDataAfterRedirect = () => {
+		clearError();
+		clearTimeout(countTimer);
+		clearTimeout(redirectTimer);
+	}
+
 	// If there is a forbidden error
 	if (!isFetching && teamError) {
 
 		if (teamError.status === 403) {
-			setTimeout(() => {
-				if (count === 0) {
-					clearTimeout();
-				}
+			countTimer = setTimeout(() => {
 				setCount(count - 1);
-			}, 1000);
+			}, 1000)
 
-			setTimeout(() => {
-				// Redirect after 10 secs...
-				history.push('/user-dashboard');
-				clearTimeout();
-				// clearError();
-			}, 10000)
-
+			if (count === 0) {
+				clearTimeout(countTimer);
+			}
 			// Display redirect message...
 			return (
 				<div style={{ textAlign: "center", fontSize: "18px", width: "100%" }}>
-					<h1 style={{ fontSize: "36px" }}>Redirect</h1>
+					<h1 style={{ fontSize: "36px" }}>Forbidden</h1>
 					<p>Uh oh! You are trying to access a page that you do not have permission to.</p>
 					<p>You will be redirected in {count} secs...</p>
-					<p>Or <Link to="/user-dashboard" onClick={() => clearError()}>click here</Link> to redirect immediately.</p>
+					<p>Or <Link to="/user-dashboard" onClick={clearDataAfterRedirect}>click here</Link> to redirect immediately.</p>
 				</div>
 			)
 		}
