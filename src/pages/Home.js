d8cbcaf3956850ea295817360d3c73850e5e80c0
Darrin Lowery <lambda@monsto.com>
2020-02-26 10:02:48 -0600

    first showing.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,14 +4,14 @@ import { Card, Button } from "antd";
 const Home = () => {
 	return (
 		<header>
-			<div class="mainbg"></div>
-			<div class="cover"></div>
-			<div class="header">
+			<div className="mainbg"></div>
+			<div className="cover"></div>
+			<div className="header">
 				<h1>[how to]</h1>
 				<h2><strong>Your</strong> how-to site about how-to</h2>
 			</div>
-			<div class="tweentxt">
-				<p><span class="logoEmbed">[how to]</span> is a community for people who like to make things.
+			<div className="tweentxt">
+				<p><span className="logoEmbed">[how to]</span> is a community for people who like to make things.
 			</p><p>Come explore, share, and make your next project with us!<br />
 				</p>
 			</div>
