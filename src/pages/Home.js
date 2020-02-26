1965c4366aea7177719d7be14eb2c9a28e2a9a48
Darrin Lowery <lambda@monsto.com>
2020-02-26 10:34:38 -0600

    colors ok.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
 			<div className="mainbg"></div>
 			<div className="cover"></div>
 			<div className="header">
-				<h1>[how to]</h1>
+				<h1>Success Begins Here.</h1>
 				<h2><strong>Your</strong> how-to site about how-to</h2>
 			</div>
 			<div className="tweentxt">
