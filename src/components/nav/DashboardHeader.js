c340e60187a43c3286854c2112a8c41d323fe4ed
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-04-08 12:19:50 -0700

    implemented notification feature

--- src/components/nav/DashboardHeader.js
+++ src/components/nav/DashboardHeader.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
+
+import { BellOutlined } from '@ant-design/icons';
 import { Layout, Avatar, Popover } from "antd";
+import { Menu, Dropdown } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+
 import { logoutUser } from "../../redux/actions/userActions";
 
 const { Header } = Layout;
@@ -21,13 +26,62 @@ function DashboardHeader(props) {
 
 	const content = (
 		<div>
-			<button onClick={handleLogout}>Logout</button>
+		<button onClick={handleLogout}>Logout</button>
 		</div>
-	);
+	);	
+		
+
+		
+	
+	let feedback = props.videos.map(item => {
+		return item.feedback
+	})
+
+	
+
+	let userFeedback = gatherFeedback(feedback);
+	console.log('userFeedback ', userFeedback)
+	
+	function gatherFeedback(arr){
+		
+		let newArray = []
+		for(let i=0; i < arr.length; i++) {
+			if(arr[i].length > 0){
+			for (let k=0; k < arr[i].length; k++) {
+				newArray.push(arr[i][k])
+				console.log('hhhhhh',arr[i][k])
+			}
+		}
+	}
+	return newArray;
+	}
+
+ 
+
+ 
+
+	const menu = (
+				<Menu>
+					{userFeedback.map(item => (
+		  			<Menu.Item>
+						<a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+			  			{item.first_name} {item.last_name} left a feedback on video {item.video_title}
+					</a>
+		  			</Menu.Item>
+					))}
+				</Menu>
+	)
 
 	return (
 		<Header className="userDashHeader">
 			<div className="userDashContentHeader">
+
+				<Dropdown overlay={menu}>
+    				<a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+					<BellOutlined style={{ fontSize: '40px' }} /> <DownOutlined />
+   					 </a>
+  				</Dropdown>,
+
 				<Popover content={content} trigger="click">
 					<Avatar size="large" icon="user" src={`${process.env.REACT_APP_S3_STORAGE_PATH}${props.avatar}`} />
 					{<p>{getInitials()}</p>}
@@ -41,12 +95,14 @@ const mapStateToProps = (state) => {
 	return {
 		fullName: `${state.User.first_name} ${state.User.last_name}`,
 		username: state.User.username,
-		avatar: state.User.avatar
+		avatar: state.User.avatar,
+		videos: state.User.videos
 	};
 };
 
 const mapActionsToProps = {
 	logoutUser,
+	
 };
 
 export default connect(mapStateToProps, mapActionsToProps)(DashboardHeader);
